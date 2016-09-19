4bf84e25d5bc80360998790b767f53c4dacd523b
Sergio Ochoa <sochoa@openkm.com>
2016-09-19 14:46:12 -0400

    Arreglando un poco las vistas con bootstrap y se añadio el ejercicio del video 7 HTTP

--- cf/7http/controller.js
+++ cf/7http/controller.js
@@ -1,6 +1,14 @@
 angular.module("MyFirstApp", [])
-        .controller("FirstController", function(scope, $http) {
-            $http.get()
+        .controller("FirstController", function ($scope, $http) {
+            $scope.posts = [];
+            // retornar un objeto promise
+            $http.get("http://jsonplaceholder.typicode.com/posts")
+                    .success(function (data) {
+                        $scope.posts = data;
+                    })
+                    .error(function () {
+
+                    });
         });
 
 
