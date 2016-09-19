b87d80c1a2ab4bcd4b9520c06575f9a8d1717062
Sergio Ochoa <sochoa@openkm.com>
2016-09-19 16:41:28 -0400

    añadiendo el ejercicio del video 9 ToDoList

---
+++ cf/9ToDoList/controller.js
@@ -0,0 +1,42 @@
+//LocalStorageModule se esta añadiendo como dependencia
+angular.module("ToDoList", ["LocalStorageModule"])
+        .controller("ToDoController", function ($scope, localStorageService) {
+            if (localStorageService.get("angular-todoList")) {
+                $scope.todo = localStorageService.get("angular-todoList");
+            } else {
+                $scope.todo = [];
+            }
+
+            $scope.newActivity = {
+                description: '',
+                date: ''
+            };
+
+            /**
+             {
+             description: 'Terminar el curso angular',
+             date: '25-09-2016'
+             }
+             */
+            /*
+            $scope.$watch(function () {
+                return $scope.newActivity;
+            }, function (newValue, oldValue) {
+                console.log(newValue);
+                console.log(oldValue);
+            });
+            */
+           
+           $scope.$watchCollection('todo',function(newValue,oldValue){
+               localStorageService.set("angular-todoList", $scope.todo);
+           });
+           
+            $scope.addActivity = function () {
+                $scope.todo.push($scope.newActivity)
+                $scope.newActivity = {};                
+            }
+
+            $scope.cleanActivity = function () {
+                $scope.todo = [];                
+            }
+        });
